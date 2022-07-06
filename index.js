3493bf8dcc59b3b103b39ed98873d02ba05f66bf
wodingnia <108749857+wodingnia@users.noreply.github.com>
2022-07-06 09:31:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const http = require('http');
 const https = require('https');
 const zlib = require('zlib');
 
-//https://ip230617622.ahcdn.com/key=vTSNHnrT08j+6xpxO7aPDw,s=,end=1657115636,limit=3/state=YsRCwKlg/referer=none,.vjav.com,.gstatic.com,.vjav.com/reftag=057661800/media=hls/ssd8/121/2/285133392.m3u8
-//http://127.0.0.1:3000/ip230617622.ahcdn.com/key=vTSNHnrT08j+6xpxO7aPDw,s=,end=1657115636,limit=3/state=YsRCwKlg/referer=none,.vjav.com,.gstatic.com,.vjav.com/reftag=057661800/media=hls/ssd8/121/2/285133392.m3u8
+//https://ip230614840.ahcdn.com/key=DvNuO9kapWb0eeFsg13AKA,s=,end=1657155164,limit=3/state=YsTcwKlg/referer=none,.vjav.com,.gstatic.com,.vjav.com/reftag=057661800/media=hls/ssd1/121/6/285562686.m3u8
+//http://127.0.0.1:3000/ip230614840.ahcdn.com/key=DvNuO9kapWb0eeFsg13AKA,s=,end=1657155164,limit=3/state=YsTcwKlg/referer=none,.vjav.com,.gstatic.com,.vjav.com/reftag=057661800/media=hls/ssd1/121/6/285562686.m3u8
 
 //http://127.0.0.1:3000/ip230617622.ahcdn.com/key=vTSNHnrT08j+6xpxO7aPDw,s=,end=1657115636,limit=3/state=YsRCwKlg/referer=none,.vjav.com,.gstatic.com,.vjav.com/reftag=057661800/media=hls/ssd8/121/2/285133392.mp4/seg-1-v1-a1.ts
+//https://cdn77-vid.xvideos-cdn.com/j5cube9omh-gu4oeDVttBw==,1657078375/videos/hls/bb/8a/80/bb8a80ab5b86036d3fde58dfdd11afa5/hls.m3u8
 
 http.createServer(onRequest).listen(5000);
 
 function onRequest(client_req, client_res) {
     console.log('serve: ' + client_req.url);
-    let cdn_location_regex = /ip(\d+).ahcdn.com/
+    let cdn_location_regex = /([\w|\d|-]*?)\.(xvideos-cdn||ahcdn)\.com/
     let cdn_location = client_req.url.match(cdn_location_regex);
     if (cdn_location == null) {
         return client_res.end()
     }
-    let real_url = client_req.url.replace(`/${cdn_location[0]}`, '')
+    cdn_location=cdn_location[0];
+    let real_url = client_req.url.replace(`/${cdn_location}`, '')
     let options = {
-        hostname: cdn_location[0],
+        hostname: cdn_location,
         port: 443,
         path: real_url,
         method: client_req.method,
@@ -26,8 +28,15 @@ function onRequest(client_req, client_res) {
         rejectUnauthorized: false,
         strictSSL: false
     };
-    options.headers.referer = 'https://vjav.com/';
-    options.headers['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36'
+    let referer='https://vjav.com/';
+    if(cdn_location.includes('xvideos')){
+        referer='https://www.xvideos.com';
+    }
+    options.headers.referer = referer;
+    options.headers['accept']='*/*';
+    options.headers['accept-encoding']='gzip, deflate, br';
+    options.headers['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36';
+    delete options.headers['upgrade-insecure-requests']
     let proxy = https.request(options, function (res) {
         if (options.path.includes(".m3u8")) {
             let headers= JSON.parse(JSON.stringify(res.headers));
@@ -42,18 +51,18 @@ function onRequest(client_req, client_res) {
                 body.push(chunk)
                 
             })
-            let hostport=/https?:\/\/(?:w{1,3}\.)?[^\s.]+(?:\.[a-z]+)*(?::\d+)?(?![^<]*(?:<\/\w+>|\/?>))/ig;
+            let hostport=/(http||https):.*?(:)\d*/ig;
             gunzip.on("end", function () {
                 let _body=Buffer.concat(body).toString();
-                console.log(`/${cdn_location[0]}`)
-                _body=_body.replace(hostport,`/${cdn_location[0]}`);
+                console.log(`/${cdn_location}`)
+                _body=_body.replace(hostport,`/${cdn_location}`);
                 client_res.write(_body)
                 client_res.end()
             })
         } else {
-//             res.headers['Access-Control-Allow-Origin']= '*';
-//             res.headers['Access-Control-Allow-Methods']= 'OPTIONS, POST, GET';
-//             res.headers['Access-Control-Max-Age']=2592000;
+            // res.headers['Access-Control-Allow-Origin']= '*';
+            // res.headers['Access-Control-Allow-Methods']= 'OPTIONS, POST, GET';
+            // res.headers['Access-Control-Max-Age']=2592000;
 
             client_res.writeHead(res.statusCode, res.headers);
             res.pipe(client_res, {
